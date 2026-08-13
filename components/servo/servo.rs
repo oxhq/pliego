2b89232e7abde289926e44e88e2bad57ed7bb5e4
David Garay <garaekz@gmail.com>
2026-08-13 09:42:09 -0700

    fix(servo): satisfy shutdown lint
    Signed-off-by: David Garay <garaekz@gmail.com>

--- components/servo/servo.rs
+++ components/servo/servo.rs
@@ -863,10 +863,8 @@ impl Drop for ServoInner {
         while self.spin_event_loop() {
             std::thread::sleep(Duration::from_micros(500));
         }
-        if let Some(handle) = self.constellation_join_handle.take() {
-            if handle.join().is_err() {
-                warn!("Failed to join the constellation thread");
-            }
+        if let Some(handle) = self.constellation_join_handle.take() && handle.join().is_err() {
+            warn!("Failed to join the constellation thread");
         }
     }
 }
