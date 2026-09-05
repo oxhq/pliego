8547be951f4bfcb2f23a6827674046a7a8785423
David Garay <garaekz@gmail.com>
2026-09-05 08:08:46 -0700

    chore: declare existing licenses on Python qualification tools

--- benchmarks/tools/test_benchmark_runtime.py
+++ benchmarks/tools/test_benchmark_runtime.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 """Exact browser adapter allowlist; not a basename or arbitrary PHP classifier."""
 
 import unittest
